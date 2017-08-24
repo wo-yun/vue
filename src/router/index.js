a7ef18af12c664b3f4a0fcf9bd195f9c769ea875
Even <wendao_3210@163.com>
2017-08-25 00:10:19 +0800

    hxw

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import index from '@/view/index'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'Hello',
+    //   component: Hello
+    // },
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
-    }
+      name: 'index',
+      component: index
+    },
   ]
 })
---
+++ src/view/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="index">
+    <ul>
+      <li></li>
+    </ul>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    data () {
+      return {
+        list: ['aaa', 'bbb']
+      }
+    },
+
+  }
+</script>
+
+<style>
+
+</style>
