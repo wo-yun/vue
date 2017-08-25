f0a12a1a0682580dc2eb8c9722cedf6b3239cc55
Even <wendao_3210@163.com>
2017-08-25 14:55:54 +0800

    hxw

--- src/view/index.vue
+++ src/view/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="index">
     <ul>
-      <li></li>
+      <li class="lf" v-for="val in list">{{ val }}</li>
     </ul>
   </div>
 </template>
@@ -18,5 +18,16 @@
 </script>
 
 <style>
-
+  #index li {
+    width: 100px;
+    line-height: 36px;
+    border: 1px solid #0C8EF1;
+    font-size: 16px;
+    color: #fff;
+    background: #0C8EF1;
+    border-radius: 4px;
+    margin: 3px 5px;
+    text-align: center;
+    cursor: pointer;
+  }
 </style>
