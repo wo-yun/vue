8500d08d0f2a396fff5a0dbb76dc92cd4f30c748
Even <wendao_3210@163.com>
2017-08-25 15:08:13 +0800

    hxw

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
-import index from '@/view/index'
 
 Vue.use(Router)
 
@@ -15,7 +14,7 @@ export default new Router({
     {
       path: '/',
       name: 'index',
-      component: index
+      component: require('@/view/index')
     },
   ]
 })
