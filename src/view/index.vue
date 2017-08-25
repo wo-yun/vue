86565dbed0ad6f6b28ba37b4aaa1f76d514ccc10
Even <wendao_3210@163.com>
2017-08-25 15:01:49 +0800

    hxw

--- src/view/index.vue
+++ src/view/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="index">
     <ul>
-      <li class="lf" v-for="val in list">{{ val }}</li>
+      <li class="lf" v-for="(val, index) in list" @click="jump(index)">{{ val }}</li>
     </ul>
   </div>
 </template>
@@ -10,16 +10,20 @@
   export default {
     data () {
       return {
-        list: ['aaa', 'bbb']
+        list: ['aaa', 'bbb', 'ccc', 'ddddddddddddddddddddddddd']
       }
     },
-
+    methods: {
+      jump (index) {
+        alert(index)
+      }
+    }
   }
 </script>
 
 <style>
   #index li {
-    width: 100px;
+    min-width: 100px;
     line-height: 36px;
     border: 1px solid #0C8EF1;
     font-size: 16px;
@@ -29,5 +33,6 @@
     margin: 3px 5px;
     text-align: center;
     cursor: pointer;
+    padding: 0 10px;
   }
 </style>
